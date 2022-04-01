6f131b11ad572cc1706b88ff56e0a65f407b9c44
Milu <miluskapajuelo@gmail.com>
2022-04-01 10:13:57 -0500

    add new function

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ import React from 'react'
 
 function Home() {
   const listItems = [1,2,3,4]
+
+  const addItem = () => {
+    const variableA = 1;
+    const variableB = 3;
+    const variableC = 2;
+
+    return variableA + variableC*variableB
+    
+  }
   return (
-    <div>Home</div>
+    <><div>Home</div><button onclick={addItem} /></>
+    
   )
 }
 
